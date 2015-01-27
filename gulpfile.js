1381147452b0a5863f0e05804492d0204880e998
phi <phi1618jp@gmail.com>
2015-01-27 19:33:34 +0900

    use gulp

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var ghelper = require('gulp-helper');
+ghelper.require();
+
+
+gulp.task('default', function() {
+    var targets = [
+        "scripts/native.js",
+        "scripts/api.js",
+        "scripts/nativeaudio.js",
+    ];
+
+	gulp.src(targets)
+		.pipe(concat('tmlib.native.js'))
+		.pipe(gulp.dest('./'))
+		.pipe(uglify())
+	    .pipe(rename({
+	      extname: '.min.js'
+	    }))
+		.pipe(gulp.dest('./'))
+		;
+});
+
--- tmlib.native.js
+++ tmlib.native.js
@@ -1,11 +1,4 @@
 /*
- * tmlib.native.js 0.0.1
- * MIT Licensed
- * 
- * Copyright (C) 2010 phi, https://twitter.com/phi_jp
- */
-
-/*
  * native.js
  */
 
